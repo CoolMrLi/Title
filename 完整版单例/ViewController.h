94bdd4c39e9b76eafc6fa25cd295d54974f3a748
qianfeng <qianfeng@LiWenjun.local>
2015-03-25 09:41:18 +0800

    Initial Commit

---
+++ 完整版单例/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  完整版单例
+//
+//  Created by qianfeng on 15/3/25.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
